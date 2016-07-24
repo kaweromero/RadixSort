38708eb8af61f7761f0a3d3e6f6f43dfeb8c6bbe
kaweromero <kaweromero@gmail.com>
2016-07-24 17:53:03 -0300

    Talk shit, get shot!

--- src/NumeroAleatorio.java
+++ src/NumeroAleatorio.java
@@ -22,7 +22,7 @@ public class NumeroAleatorio {
 		for (int i = 0; i < quantNumeros; i++) {
 
 			array[i] = Long.toString(number = (long) Math.floor(Math.random() * 9_000_000_000L) + 1_000_000_000L);
-			// System.out.println(number);
+			 System.out.println(number);
 		}
 
 		return array;
--- src/RadixSortido.java
+++ src/RadixSortido.java
@@ -1,50 +1,77 @@
 
 public class RadixSortido {
 
-	
 	NumeroAleatorio geraNum = new NumeroAleatorio();
-	String[] arrayNum = new String[10];
-	
-	
-	public int[] separaDigito(int quantNumero){
-		//1000000000 a 9999999999
-		
-		arrayNum = geraNum.geraNumeros(quantNumero);
-		
-		for (int j = 0; j<= quantNumero;j++){
-			
-				
-			for(int i = 0; i<10;i++){
-					
-				arrayNum[i].charAt(i);
-				
-			}
-			
-			
-		}
-		
-	
-		
-		
-		
-		/*public void split(String expressao) {
+	int tamanhoArray = 0;
+	String[] arrayNum = new String[tamanhoArray];
+	String[] aOrdenar0 = new String[tamanhoArray];
+	String[] aOrdenar1 = new String[tamanhoArray];
+	String[] aOrdenar2 = new String[tamanhoArray];
+	String[] aOrdenar3 = new String[tamanhoArray];
+	String[] aOrdenar4 = new String[tamanhoArray];
+	String[] aOrdenar5 = new String[tamanhoArray];
+	String[] aOrdenar6 = new String[tamanhoArray];
+	String[] aOrdenar7 = new String[tamanhoArray];
+	String[] aOrdenar8 = new String[tamanhoArray];
+	String[] aOrdenar9 = new String[tamanhoArray];
 
-			for (int i = 0; i < expressao.length(); i++) {
-				if (expressao.charAt(i) == '(') {
-					fila1.adicionarFila(1);
-					//String abre = "(";
-					//inserir(fila1, abre);
-					//stack.addElement(abre);
-				}*/
-				
+	public int[] separaDigito(int quantNumero, int posicao) {
+		// 1000000000 a 9999999999
+		this.tamanhoArray = quantNumero;
+		arrayNum = geraNum.geraNumeros(quantNumero);
+		String[] AUX = new String[quantNumero];
+
+		// for (int j = 9; j >= 0 ;j--){
+		// }
+
+		for (int i = 0; i < quantNumero; i++) {
+
+			System.out.println(arrayNum[i].charAt(posicao));
+			AUX[i] = Character.toString(arrayNum[i].charAt(posicao));
+
+		}
+		// Armazenar os valores em um array e ordenar aqui!!!
+
+		if (posicao == 0) {
+			this.aOrdenar0 = AUX;
+		}
+		if (posicao == 1) {
+			this.aOrdenar0 = AUX;
+		}
+		if (posicao == 2) {
+			this.aOrdenar0 = AUX;
+		}
+		if (posicao == 3) {
+			this.aOrdenar0 = AUX;
+		}
+		if (posicao == 4) {
+			this.aOrdenar0 = AUX;
+		}
+		if (posicao == 5) {
+			this.aOrdenar0 = AUX;
+		}
+		if (posicao == 6) {
+			this.aOrdenar0 = AUX;
+		}
+		if (posicao == 7) {
+			this.aOrdenar0 = AUX;
+		}
+		if (posicao == 8) {
+			this.aOrdenar0 = AUX;
+		}
+		if (posicao == 9) {
+			this.aOrdenar0 = AUX;
+		}
+
 		return null;
-		
+
 	}
-	
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		RadixSortido ex = new RadixSortido();
+		ex.separaDigito(20, 9);
+		// System.out.println(ex.aOrdenar[0]);
 	}
 
 }
