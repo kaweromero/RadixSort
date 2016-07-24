8a3475e2442dbd38fadfc8bafbbcf234519f0466
kaweromero <kaweromero@gmail.com>
2016-07-24 18:27:27 -0300

    Наш Советский Союз покоряет весь мир

--- src/RadixSortido.java
+++ src/RadixSortido.java
@@ -15,54 +15,56 @@ public class RadixSortido {
 	String[] aOrdenar8 = new String[tamanhoArray];
 	String[] aOrdenar9 = new String[tamanhoArray];
 
-	public int[] separaDigito(int quantNumero, int posicao) {
+	public int[] separaDigito(int quantNumero) {
 		// 1000000000 a 9999999999
 		this.tamanhoArray = quantNumero;
 		arrayNum = geraNum.geraNumeros(quantNumero);
-		String[] AUX = new String[quantNumero];
+		
+		
+		for (int posicao= 9; posicao >= 0; posicao--) {
+			String[] AUX = new String[quantNumero];
 
-		// for (int j = 9; j >= 0 ;j--){
-		// }
+			for (int i = 0; i < quantNumero; i++) {
 
-		for (int i = 0; i < quantNumero; i++) {
+				System.out.println(arrayNum[i].charAt(posicao));
+				AUX[i] = Character.toString(arrayNum[i].charAt(posicao));
 
-			System.out.println(arrayNum[i].charAt(posicao));
-			AUX[i] = Character.toString(arrayNum[i].charAt(posicao));
+			}
+			// Armazenar os valores em um array e ordenar aqui!!!
+
+			if (posicao == 0) {
+				this.aOrdenar0 = AUX;
+			}
+			if (posicao == 1) {
+				this.aOrdenar1 = AUX;
+			}
+			if (posicao == 2) {
+				this.aOrdenar2 = AUX;
+			}
+			if (posicao == 3) {
+				this.aOrdenar3 = AUX;
+			}
+			if (posicao == 4) {
+				this.aOrdenar4 = AUX;
+			}
+			if (posicao == 5) {
+				this.aOrdenar5 = AUX;
+			}
+			if (posicao == 6) {
+				this.aOrdenar6 = AUX;
+			}
+			if (posicao == 7) {
+				this.aOrdenar7 = AUX;
+			}
+			if (posicao == 8) {
+				this.aOrdenar8 = AUX;
+			}
+			if (posicao == 9) {
+				this.aOrdenar9 = AUX;
+			}
 
 		}
-		// Armazenar os valores em um array e ordenar aqui!!!
-
-		if (posicao == 0) {
-			this.aOrdenar0 = AUX;
-		}
-		if (posicao == 1) {
-			this.aOrdenar0 = AUX;
-		}
-		if (posicao == 2) {
-			this.aOrdenar0 = AUX;
-		}
-		if (posicao == 3) {
-			this.aOrdenar0 = AUX;
-		}
-		if (posicao == 4) {
-			this.aOrdenar0 = AUX;
-		}
-		if (posicao == 5) {
-			this.aOrdenar0 = AUX;
-		}
-		if (posicao == 6) {
-			this.aOrdenar0 = AUX;
-		}
-		if (posicao == 7) {
-			this.aOrdenar0 = AUX;
-		}
-		if (posicao == 8) {
-			this.aOrdenar0 = AUX;
-		}
-		if (posicao == 9) {
-			this.aOrdenar0 = AUX;
-		}
-
+		
 		return null;
 
 	}
@@ -70,8 +72,10 @@ public class RadixSortido {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RadixSortido ex = new RadixSortido();
-		ex.separaDigito(20, 9);
-		// System.out.println(ex.aOrdenar[0]);
+		ex.separaDigito(5);
+		
+		//System.out.println("HUE1 "+ex.aOrdenar8[3]);
+		//System.out.println("HUE2 "+ex.aOrdenar8[4]);
 	}
 
 }
