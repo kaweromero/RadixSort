362d4480d1ebd240e72691dcffcc307004d539b8
kaweromero <kaweromero@gmail.com>
2016-07-27 22:48:23 -0300

    Stalin matou foi pouco :v

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -32,23 +32,6 @@ public class BubbleSort {
 
 	private static void bubbleSort(int[] intArray) {
 
-		/*
-		 * In bubble sort, we basically traverse the array from first to
-		 * array_length - 1 position and compare the element with the next one.
-		 * Element is swapped with the next element if the next element is
-		 * greater.
-		 *
-		 * Bubble sort steps are as follows.
-		 *
-		 * 1. Compare array[0] & array[1] 2. If array[0] > array [1] swap it. 3.
-		 * Compare array[1] & array[2] 4. If array[1] > array[2] swap it. ... 5.
-		 * Compare array[n-1] & array[n] 6. if [n-1] > array[n] then swap it.
-		 *
-		 * After this step we will have largest element at the last index.
-		 *
-		 * Repeat the same steps for array[1] to array[n-1]
-		 * 
-		 */
 
 		int n = intArray.length;
 		int temp = 0;
--- src/RadixSort.java
+++ src/RadixSort.java
@@ -106,6 +106,28 @@ public class RadixSort {
 		return array;
 	}
 
+	private static void bubbleSort(long[] array,int place) {
+
+
+		int n = array.length;
+		long temp = 0;
+		//int digit = getDigit(array[i], place);
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+
+				if (array[j - 1] > array[j]) {
+					// swap the elements!
+					temp = array[j - 1];
+					array[j - 1] = array[j];
+					array[j] = temp;
+				}
+
+			}
+		}
+
+	}
+	
     private static int getDigit(long value, int digitPlace){
         return (int) ((value/digitPlace ) % 10);
     }
